16b0369ec6e521fe53d1ecaa49fb7dbbd0a4b0a3
gregoryk07 <74604654+gregoryk07@users.noreply.github.com>
2024-01-24 14:47:55 +0100

    Whole website update
    gregorykahub v3

---
+++ scriptafter.js
@@ -0,0 +1,26 @@
+function detectOS() {
+	let userAgent = window.navigator.userAgent,
+		platform = window.navigator.platform,
+		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+		os = null;
+
+	if (macosPlatforms.indexOf(platform) !== -1) {
+		os = 'Mac OS';
+	} else if (iosPlatforms.indexOf(platform) !== -1) {
+		os = 'iOS';
+	} else if (windowsPlatforms.indexOf(platform) !== -1) {
+		os = 'Windows';
+	} else if (/Android/.test(userAgent)) {
+		os = 'Android';
+	} else if (!os && /Linux/.test(platform)) {
+		os = 'Linux';
+	}
+
+	return os;
+}
+if(detectOS() != 'Windows'){
+    document.getElementById('panelw').style.display = 'none';
+    document.getElementById('panelo').style.display = 'block';
+}
